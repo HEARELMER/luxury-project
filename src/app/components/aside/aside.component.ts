11c748c8d62851172d138ba5ea9f540f54aa4fa1
HEARELMER <hearelmer2005@gmail.com>
2024-03-01 13:23:16 -0500

    nuevo

--- src/app/components/aside/aside.component.ts
+++ src/app/components/aside/aside.component.ts
@@ -9,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AsideComponent {
 
+  paquetes:string[]=[
+    'Luxury Express 03D/02N',
+    'Luxury Tradicional 03D/02N',
+    'Luxury Señorial 04D/03N',
+    'Luxury donde nace el Perú 05D/04N',
+   'Luxury memorable 06D/05N'
+  ]
+  tours:string[]=[
+    'City Tours Tradicional',
+    'Libertad Americana (Wari)',
+    'Primer Hombre Andino (Huanta)',
+     'Ruta del Inca - VilcasHuamán',
+     'Los Mártires de Cangallo',
+     'Joya Escondida - Millpu',
+     'Grietas de Qorihuillca',
+     'Piscina termal - Colpa',
+     'Eco Aventura',
+     'Bosque de piedras - Huaraca',
+     'Mirador Halcón - Columpio Extremo',
+     'Aguas Termales - Pachapupum',
+     'Laguna Azul',
+     'Laguna Verdeqocha',
+     'Valle de cascadas - Sarhua',
+    ]
 }
--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -5,16 +5,47 @@ import { CardComponent } from '../../components/card/card.component';
 import { CarouselComponent } from '../../components/carousel/carousel.component';
 import { ContentCardsComponent } from '../../components/content-cards/content-cards.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { defer } from 'rxjs';
+import { AsideComponent } from '../../components/aside/aside.component';
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports:[ButtonsComponent, CardComponent,CarouselComponent, ContentCardsComponent, FooterComponent],
+  imports:[ButtonsComponent, AsideComponent,
+    CardComponent,CarouselComponent, ContentCardsComponent, FooterComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
 })
 export class InicioComponent {
 
   dataCard:any[] =[
+    {
+        namePlace: 'Aguas Turquesas',
+        image:'https://fresac.pe/wp-content/uploads/2021/04/Millpu.jpg',
+        duration:'Full day',
+        hour:'9:30 - 5:00 pm', 
+        price: '45'
+    },
+    {
+        namePlace: 'Catarata Pumacocha',
+        image:'https://denomades.s3.us-west-2.amazonaws.com/blog/wp-content/uploads/2020/09/13163132/70928067_103581854376236_9028388690644172800_n.jpg',
+        duration:'Full day',
+        hour:'9:30 - 5:00 pm', 
+        price: '15'
+    },
+    {
+        namePlace: 'VilcasHuaman',
+        duration:'Full day',
+        image:'./assets/imagenes/vilcas.webp',
+        hour:'9:30 - 5:00 pm', 
+        price: '15'
+    },
+    {
+        namePlace: 'Pampas de Ayacucho',
+        image:'./assets/imagenes/quinua-full.webp',
+        duration:'Full day',
+        hour:'9:30 - 5:00 pm', 
+        price: '55'
+    },
     {
         namePlace: 'Aguas Turquesas',
         image:'https://fresac.pe/wp-content/uploads/2021/04/Millpu.jpg',
